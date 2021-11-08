86a17658a8eea60fa183b76daae03238821619dc
mihkelvaher <mihkel.vaher@industry62.com>
2021-11-08 17:03:23 +0200

    React-start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="my-text">adsadasdas</div>
+      <div className="my-text">asdaeweqwe</div>
+      <button className="main-button">Vajuta mind</button>
+    </div>
+  );
+}
+
+export default App;
